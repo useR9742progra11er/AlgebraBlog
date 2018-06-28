f9dc829e412ac0db2f8f94cb9f23a67cba13a2b8
useR9742progra11er <hrvoje.molnar.15@gmail.com>
2018-06-28 17:48:07 +0200

    laravel laratrust posts

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(SentinelDatabaseSeeder::class);
+        $this->call(LaratrustSeeder::class);
     }
 }
