b15c5974b347fc6145d7bcd53bcfffc4578613fe
useR9742progra11er <hrvoje.molnar.15@gmail.com>
2018-07-02 17:32:17 +0200

    Added new changes to project model comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,26 +1,84 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
-use App\Presenters\DatePresenter;
-
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Comment extends Model
 {
-  use DatePresenter;
+	use Sluggable;
 
-  // fields can be filled
-  protected $fillable = ['body', 'user_id', 'post_id'];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['body', 'user_id', 'post_id'];
 
-  public function post()
-  {
-    return $this->belongsTo('App\Post');
-  }
+	/**
+     * Save new comment
+     *
+     * @param array $data
+	 * @return object Post
+     */
+	public function saveComment($data)
+	{
+		return $this->create($data);
+	}
 
-  public function user()
-  {
-    return $this->belongsTo('App\User');
-  }
+	/**
+     * Update comment
+     *
+     * @param array $data
+	 * @return void
+     */
+	public function updateComment($data)
+	{
+		$this->update($data);
+	}
+
+	/**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+	/**
+     * Return the user relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+	public function user()
+	{
+		return $this->belongsTo('App\Models\User');
+	}
+
+	/**
+     * Return the comment relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\belongsTo
+     */
+     public function post()
+     {
+      return $this->belongsTo('App\Post');
+    }
+
+    /**
+       * Return the post relationship.
+       *
+       * @return \Illuminate\Database\Eloquent\Relations\HasMany
+       */
+  	public function comments()
+  	{
+  		return $this->hasMany('App\Models\Comment');
+  	}
 }
