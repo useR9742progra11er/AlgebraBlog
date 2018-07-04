a06ba55415f23f69cf1a970799e39dee0b5322bc
useR9742progra11er <hrvoje.molnar.15@gmail.com>
2018-07-04 17:42:10 +0200

    added comment update

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,3 +1,6 @@
+<?php
+ 		date_default_timezone_set('Europe/Zagreb');
+?>
 @extends('Centaur::layout')
 
 @section('title')
@@ -18,34 +21,33 @@ Algebra Blog | {{ $post->title }}
 	<div class="row">
 		<div class="col-sm-12">
 			@if(Sentinel::check())
-				//tu prikazi formu za komentiranje
-
-				<h3>Comments</h3>
-				@if (Auth::check())
-  				@include('includes.errors')
-  					{{ Form::open(['route' => ['comments.store'], 'method' => 'POST']) }}
-  			<p>{{ Form::textarea('body', old('body')) }}</p>
-  					{{ Form::hidden('post_id', $post->id) }}
-  			<p>{{ Form::submit('Send') }}</p>
-				{{ Form::close() }}
-				@endif
-				@forelse ($post->comments as $comment)
-  			<p>{{ $comment->user->name }} {{$comment->created_at}}</p>
-  			<p>{{ $comment->body }}</p>
-  			<hr>
-				@empty
-  		<p>This post has no comments</p>
-			<span>{{$post->comments->count()}} {{ str_plural('comment', $post->comments->count()) }}</span>
-			@endforelse
+				<!--tu prikazi formu za komentiranje-->
+			</br>
+			</br>
+			<h3><b>Add new Comment</b></h3>
+			<?php
+			echo "<form>
+					<input type='hidden' name='user_id' value='Anonymous'>
+					<input type='hidden' name='created_at' value='".date('Y-m-d H:i:s')."'>
+					<textarea name='body'></textarea><br>
+					<button type='submit' name='submit'>Comment</button>
+			</form>";
+			?>
 
 			@else
-				tu prikazi link za login
+				<!--tu prikazi link za login-->
+				<a href="http://localhost:8000/login">Login</a>
 			@endif
 		</div>
 	</div>
 	<div class="row">
 		<div class="col-sm-12">
-			ispis svih komentara $post->comment
+			<!--ispis svih komentara $post->comment-->
+		</br>
+			<h3><b>Comments</b></h3>
+		</br>
+			<p>This post has no comments</p>
+			<span>{{$post->comments->count()}} {{ str_plural('comment', $post->comments->count()) }}</span>
 		</div>
 	</div>
 @endsection
